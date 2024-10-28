9207d64f78a87d7646dfadfaaf4e1115a17ff203
Carlos Ceballos <carlosgceballosq@gmail.com>
2024-10-27 23:29:44 -0400

    programa de entrada de datos

--- entrada.cpp
+++ entrada.cpp
@@ -20,5 +20,21 @@ int main(){
     return 0;
   }
 
+    cout<<"Cuantos datos desea ingresar?"<<endl;
+    cin>>x;
+    while(x>0){
+      cout<<"Ingrese el nombre: ";
+      cin>>person.name;
+      cout<<"Ingrese la cedula: ";
+      cin>>person.cedula;
+      cout<<"Ingrese el email: ";
+      cin>>person.email;
+      cout<<endl;
+
+      output<<person.name<<"\t"<<person.cedula<<"\t"<<person.email<<endl;
+
+      x--;
+    }
+
   return 0;
 }
